9cf6da77309f65fc590f2ab5655351872743c075
tokarro666 <tokarro666@gmail.com>
2022-06-11 16:15:04 +0200

    zmiana tekstu w pliku server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Testowanie jest super SUPER!');
+  res.end('Nie wiem jak to zrobić parkingAPP');
 }
 
 const server = http.createServer(requestListener);
